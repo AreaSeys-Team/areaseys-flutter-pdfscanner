5e555c87e1b7fdfe71333f7266212caa7961c021
Santi Hoyos <santiago.hoyos93@gmail.com>
2019-11-04 10:33:25 +0100

    added spanish messages and trimMemory callback again.

--- android/src/main/java/com/areaseys/pdfscanner/pdfscanner/ScanActivity.java
+++ android/src/main/java/com/areaseys/pdfscanner/pdfscanner/ScanActivity.java
@@ -2,6 +2,7 @@ package com.areaseys.pdfscanner.pdfscanner;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.content.ComponentCallbacks2;
 import android.content.Intent;
@@ -132,6 +133,56 @@ public class ScanActivity extends AppCompatActivity implements IScanner, Compone
         }
         if (bitmap != null) {
             postImagePick(bitmap, uri);
+        }
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        switch (level) {
+            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
+      /*
+         Release any UI objects that currently hold memory.
+
+         The user interface has moved to the background.
+      */
+                break;
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
+      /*
+         Release any memory that your app doesn't need to run.
+
+         The device is running low on memory while the app is running.
+         The event raised indicates the severity of the memory-related event.
+         If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
+         begin killing background processes.
+      */
+                break;
+            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
+            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
+      /*
+         Release as much memory as the process can.
+
+         The app is on the LRU list and the system is running low on memory.
+         The event raised indicates where the app sits within the LRU list.
+         If the event is TRIM_MEMORY_COMPLETE, the process will be one of
+         the first to be terminated.
+      */
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.low_memory)
+                        .setMessage(R.string.low_memory_message)
+                        .create()
+                        .show();
+                break;
+            default:
+      /*
+        Release any non-critical data structures.
+
+        The app received an unrecognized memory level value
+        from the system. Treat this as a generic low-memory message.
+      */
+                break;
         }
     }
 
